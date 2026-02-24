30aad5aba2f4e5d2e68d87814244ff8de575070c
vmuro <victor.muniz.rosa@outlook.com>
2026-02-24 08:36:55 -0300

    minor change in constructor

--- generators/axon-java/app/index.js
+++ generators/axon-java/app/index.js
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-var Generator = require('yeoman-generator');
+var Generator = require('yeoman-generator').default || require('yeoman-generator');
 var slugify = require('slugify')
 
 
